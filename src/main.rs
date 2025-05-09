4115b6a84cadc5f7cf73d5a027f264f238e78e97
Daniel Mancebo <dama@bang-olufsen.dk>
2025-05-09 23:04:59 +0200

    The type builting: exceutable files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,70 @@
+use anyhow::{Context, Result};
+use regex::Regex;
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use std::str::FromStr;
 use std::{
     io::{self, Write},
     process::ExitCode,
 };
 
-use regex::Regex;
-
 static BUILTIN_CMDS: &'static [&'static str] = &["echo", "exit", "type"];
+
+enum CommandType {
+    Unknown,
+    Builtin,
+    Executable(PathBuf),
+}
+
+impl FromStr for CommandType {
+    type Err = ();
+
+    fn from_str(cmd: &str) -> Result<CommandType, Self::Err> {
+        if BUILTIN_CMDS.contains(&cmd) {
+            Ok(CommandType::Builtin)
+        } else if let Ok(Some(executable)) = is_executable(cmd) {
+            Ok(CommandType::Executable(executable))
+        } else {
+            Ok(CommandType::Unknown)
+        }
+    }
+}
+
+fn is_executable(cmd: &str) -> Result<Option<PathBuf>> {
+    for path in split_path() {
+        for f in fs::read_dir(&path).context("Directory in PATH cannot be read")? {
+            if f?
+                .path()
+                .to_str()
+                .expect("File should be a UTF-8 valid string")
+                .ends_with(cmd)
+            {
+                return Ok(Some(path));
+            }
+        }
+    }
+    Ok(None)
+}
+
+fn split_path() -> Vec<PathBuf> {
+    let path: String = env::var("PATH").expect("PATH environment variable should always exist");
+    let mut paths: Vec<PathBuf> = Vec::new();
+    for p in path.split(":") {
+        paths.push(PathBuf::from(&p));
+    }
+    paths
+}
+
+fn handle_type_cmd(param: &str) {
+    if BUILTIN_CMDS.contains(&param) {
+        println!("{} is a shell builtin", param);
+    } else if let Ok(Some(executable)) = is_executable(param) {
+        println!("{} is {}", param, executable.to_str().unwrap());
+    } else {
+        println!("{}: not found", param);
+    }
+}
 
 fn main() -> ExitCode {
     let exit_re: Regex = Regex::new(r"exit ([0-9]+)").unwrap();
@@ -28,11 +87,7 @@ fn main() -> ExitCode {
         } else if let Some(caps) = echo_re.captures(&input) {
             println!("{}", caps[1].trim());
         } else if let Some(caps) = type_re.captures(&input) {
-            if BUILTIN_CMDS.contains(&caps[1].trim()) {
-                println!("{} is a shell builtin", caps[1].trim());
-            } else {
-                println!("{}: not found", caps[1].trim());
-            }
+            handle_type_cmd(caps[1].trim());
         } else {
             println!("{}: command not found", input.trim());
         }
