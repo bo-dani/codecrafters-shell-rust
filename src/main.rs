7060214a7c49835d6a17aeb78e2556d9cc17f547
Daniel Mancebo <dama@bang-olufsen.dk>
2025-05-09 21:38:32 +0200

    The type builtin: builtins

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ use std::{
 
 use regex::Regex;
 
+static BUILTIN_CMDS: &'static [&'static str] = &["echo", "exit"];
+
 fn main() -> ExitCode {
     let exit_re: Regex = Regex::new(r"exit ([0-9]+)").unwrap();
     let echo_re: Regex = Regex::new(r"echo (.+)").unwrap();
+    let type_re: Regex = Regex::new(r"type (.+)").unwrap();
 
     loop {
         print!("$ ");
@@ -24,6 +27,12 @@ fn main() -> ExitCode {
             );
         } else if let Some(caps) = echo_re.captures(&input) {
             println!("{}", caps[1].trim());
+        } else if let Some(caps) = type_re.captures(&input) {
+            if BUILTIN_CMDS.contains(&caps[1].trim()) {
+                println!("{} is a shell builtin", caps[1].trim());
+            } else {
+                println!("{}: not found", caps[1].trim());
+            }
         } else {
             println!("{}: command not found", input.trim());
         }
